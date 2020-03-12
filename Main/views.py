26743b329671926fec65fb8750718077659a4f56
Alex <alex_govr@mail.ru>
2020-03-12 21:32:49 +0300

    upd: pumps_view

--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .forms import Pump_choose_form
 
 # Create your views here.
 
 def pumps_view(request):
-    pass
+    form = Pump_choose_form()
+    print(form.choices)
+    return render(request,'main/main_page.html', {'form': form})
